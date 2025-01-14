4e2d12bfdc9643f87635c82528c9875293153c06
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-14 14:01:28 +0600

    Responsive banner for all device

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -12,13 +12,13 @@ const Banner = () => {
 
             <div className='max-w-[90%] xl:max-w-[1200px] mx-auto relative'>
 
-                <div className='text-center flex items-center justify-between gap-10'>
+                <div className='text-center flex flex-col sm:flex-row items-center justify-between gap-10'>
 
-                    <div className='bg-pink-500 rounded-full h-[250px]'>
-                        <img className='h-[400px] -mt-20 mx-auto' src={dog} alt="" />
+                    <div className='bg-pink-500 rounded-full lg:h-[250px] hidden lg:block'>
+                        <img className='lg:h-[400px] -mt-20 mx-auto' src={dog} alt="" />
                     </div>
                     
-                    <div>
+                    <div className='py-20 sm:pt-36 lg:py-0'>
                         <h1 className='text-center text-4xl max-w-2xl uppercase font-russo'>Give a Paw, Gain a Lifetime of Love</h1>
 
                         <p className={`sm:max-w-lg max-w-[90%] mx-auto text-center mt-3 text-lg ${theme === 'light' ? 'text-gray-700' : 'text-gray-400'}`}>Adopting a pet is an act of kindness. Take the leap, find a friend, and build a bond that lasts forever. Your journey of unconditional love starts here.</p>
@@ -28,8 +28,8 @@ const Banner = () => {
                         </Link>
                     </div>
 
-                    <div className='bg-yellow-400 rounded-full h-[250px]'>
-                        <img className='h-[400px] -mt-16 mx-auto' src={cat} alt="" />
+                    <div className='bg-yellow-400 rounded-full sm:h-[250px]'>
+                        <img className='sm:h-[400px] -mt-20 mx-auto' src={cat} alt="" />
                     </div>
 
                 </div>
