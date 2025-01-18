0efa8657eb75aa4e9705e1e8073c4446c25f2e48
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-18 12:40:37 +0600

    useAxiosSecure complete

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios"
+import useAuth from "./useAuth"
+import { useNavigate } from "react-router"
+import { useEffect } from "react"
 
 const axiosSecure = axios.create({
     baseURL : import.meta.env.VITE_API_KEY,
@@ -6,6 +9,22 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const { logOut } = useAuth()
+    const navigate = useNavigate()
+    useEffect(() => {
+        axiosSecure.interceptors.response.use(
+        res => {
+            return res
+        },
+        async error => {
+            if (error.response.status === 401 || error.response.status === 403) {
+            await logOut()
+            navigate('/login')
+            }
+            return Promise.reject(error)
+        }
+        )
+    }, [logOut, navigate])
     return axiosSecure;
 }
 
