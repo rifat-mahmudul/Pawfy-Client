ce4e55497849d432299087e9516c81c70c295b45
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-19 09:33:46 +0600

    donation campaign pages done

--- src/Pages/DonationCampaign.jsx
+++ src/Pages/DonationCampaign.jsx
@@ -1,10 +1,87 @@
+import CampaignCard from "@/components/Card/CampaignCard";
+import SkeletonLoader from "@/components/Loader/SkeletonLoader";
+import useAxiosPublic from "@/Hooks/useAxiosPublic";
+import HelmetTitle from "@/Shared/HelmetTitle";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const DonationCampaign = () => {
-    return (
-        <section className="pt-20">
-            <h1>This is Pet Donation Campaign page..</h1>
-        </section>
-    )
-}
+    const axiosPublic = useAxiosPublic();
 
-export default DonationCampaign
+    const { ref, inView } = useInView();
+    const [search, setSearch] = useState("");
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+    } = useInfiniteQuery({
+        queryKey: ["allDonationCampaign", search],
+        queryFn: async () => {
+            const { data } = await axiosPublic(
+                `/donationCampaigns?search=${search}`
+            );
+            return data;
+        },
+        getNextPageParam: (lastPage, allPages) => {
+            if (lastPage.length === 0) return undefined;
+            return allPages.length + 1;
+        },
+    });
+
+    const donationCampaigns = data?.pages.flat() || [];
+    const sortedDonationCampaigns = donationCampaigns.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    if (inView && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+    }
+
+    return (
+        <section className="pt-20 pb-16">
+            <HelmetTitle title="Pet Listing"></HelmetTitle>
+
+            <div className="max-w-[90%] xl:max-w-[1200px] mx-auto">
+
+                <input
+                    type="text"
+                    className="border border-purple-500 p-3 w-full rounded-lg outline-0 focus:border-2 bg-inherit"
+                    placeholder="Search by pet's name"
+                    onChange={(e) => setSearch(e.target.value)}
+                />  
+
+                {/* Content Section */}
+                <div>
+                    {isFetching && !isFetchingNextPage ? (
+                        <SkeletonLoader></SkeletonLoader>
+                    ) : sortedDonationCampaigns.length === 0 ? (
+                        <p className="flex items-center justify-center flex-col text-3xl text-red-500 min-h-[calc(100vh-200px)] font-bold">
+                            NO DATA FOUND
+                        </p>
+                    ) : (
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 mt-8">
+                            {sortedDonationCampaigns.map((campaign) => (
+                                <CampaignCard key={campaign._id} campaign={campaign}></CampaignCard>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
+                {/* Infinite Scroll Trigger */}
+                {hasNextPage && (
+                    <div ref={ref} className="text-center mt-8">
+                        {isFetchingNextPage ? (
+                            <p>Loading more...</p>
+                        ) : (
+                            <p>Scroll down to load more...</p>
+                        )}
+                    </div>
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default DonationCampaign;
--- src/components/Card/CampaignCard.jsx
+++ src/components/Card/CampaignCard.jsx
@@ -1,18 +1,32 @@
 import PropTypes from 'prop-types'
-import { IoLocationOutline } from "react-icons/io5";
 import { FaEye } from "react-icons/fa";
 import { Link } from 'react-router';
 
 const CampaignCard = ({campaign}) => {
 
-    const {image, maximumDonation, sortDescription, longDescription,lastDate, _id} = campaign;
+    const {image, petName, maximumDonation, _id} = campaign;
 
     return (
         <div className='text-center w-[270px] rounded-lg transition hover:scale-105'>
             <img className='h-[220px] w-[270px] rounded-t-lg' src={image} alt="" />
 
-            <h1 className='font-bold text-lg'>Maximum Donation : {maximumDonation}</h1>
-            <h1 className='font-bold text-lg'>Maximum Donation : {maximumDonation}</h1>
+            <div className='bg-[#80808042] rounded-b-lg px-1 pb-2'>
+                <div className='flex items-center justify-between pt-3'>
+                    <h1 className='font-bold text-lg'>{petName}</h1>
+                    <h1>Maximum Donation : $ {maximumDonation}</h1>
+                </div>
+
+                <div className='flex items-center justify-between mt-2'>
+                    <h1>Donated Amount : $ {'0'}</h1>
+                    
+                    <Link to={`/pet-details/${_id}`}>
+                        <button className='flex gap-1 items-center border border-purple-500 p-2 rounded-lg hover:bg-purple-500 hover:text-white transition-[0.5s]'>
+                            <p><FaEye /></p>
+                            <p>Details</p>
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
