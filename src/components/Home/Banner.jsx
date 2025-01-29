d29501e2940903d9d8fa6646e7e909b3254813d3
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-29 21:28:12 +0600

    banner modify

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -13,8 +13,8 @@ const Banner = () => {
 
                 <div className='flex flex-col sm:flex-row items-center justify-between'>
                     
-                    <div className='pt-20 sm:pt-36 lg:py-0 mb-14 sm:w-[48%]'>
-                        <h1 className='sm:text-5xl text-3xl max-w-2xl uppercase font-russo'>Give a Paw, Gain a Lifetime of Love</h1>
+                    <div className='pt-20 sm:pt-36 lg:py-0 mb-14 sm:w-[55%]'>
+                        <h1 className={`sm:text-5xl text-3xl max-w-2xl uppercase font-russo ${theme === 'light' ? 'text-gray-700' : 'text-gray-100'}`}>Give a Paw, Gain a Lifetime of Love</h1>
 
                         <p className={`mx-auto mt-3 text-lg ${theme === 'light' ? 'text-gray-600' : 'text-gray-400'}`}>Adopting a pet is an act of kindness. Take the leap, find a friend, and build a bond that lasts forever. Your journey of unconditional love starts here.</p>
 
@@ -23,7 +23,7 @@ const Banner = () => {
                         </Link>
                     </div>
 
-                    <div className='bg-[#80008084] rounded-full rounded-tr-lg rounded-b-lg sm:h-[350px] sm:w-[40%]'>
+                    <div className='sm:h-[350px] sm:w-[40%]'>
                         <img className='sm:h-[450px] -ml-5 -mt-10 mx-auto' src={cat} alt="" />
                     </div>
 
