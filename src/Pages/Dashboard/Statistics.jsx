b4785d95d6c19065d0b2324fa0ed4370129b4f12
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-31 19:18:51 +0600

    Cart and Calender show

--- src/Pages/Dashboard/Statistics.jsx
+++ src/Pages/Dashboard/Statistics.jsx
@@ -1,5 +1,8 @@
+import Carts from "@/components/Dashboard/DashNav/Carts";
 import useAxiosSecure from "@/Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
 const Statistics = () => {
 
@@ -33,7 +36,7 @@ const Statistics = () => {
 
     
   return (
-    <section>
+    <section className="pb-16">
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-10">
 
             <div className="bg-gradient-to-r from-purple-700 via-purple-500 to-purple-200 p-4 rounded-lg text-center shadow-xl shadow-purple-900">
@@ -49,6 +52,20 @@ const Statistics = () => {
             <div className="bg-gradient-to-r from-orange-700 via-orange-500 to-orange-200 p-4 rounded-lg text-center shadow-xl shadow-orange-900">
                 <h1 className="text-4xl text-white font-bold">Total Donation</h1>
                 <h1 className="mt-2 text-2xl font-semibold text-white">{totalDonation}$</h1>
+            </div>
+
+        </div>
+
+        {/* carts and calendar */}
+        <div className="mt-16 flex sm:flex-row flex-col gap-16 sm:gap-0 justify-between w-full">
+
+            <div>
+                <Carts></Carts>
+                <h1 className="text-center mt-2 text-xl font-bold">Donation Report</h1>
+            </div>
+
+            <div>
+                <Calendar />
             </div>
 
         </div>
